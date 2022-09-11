774fc3cb46356d1f76bb854b3583e81a2da92fd8
zhafiri19 <zhafirifarhan5@gmail.com>
2022-09-11 15:07:06 +0700

    penambahan skills

--- script.js
+++ script.js
@@ -30,3 +30,34 @@ let section = document.querySelectorAll('section');
                 }
             })
         };
+
+
+// progress skills bar
+const skillsSection = document.getElementById("progess-skills");
+const progressBars = document.querySelectorAll(".skills__bar");
+
+function showProgress() {
+  progressBars.forEach((progressBar) => {
+    const value = progressBar.dataset.progress;
+    progressBar.style.opacity = 1;
+    progressBar.style.width = `${value}%`;
+  });
+}
+
+function hideProgress() {
+  progressBars.forEach((p) => {
+    p.style.opacity = 0;
+    p.style.width = 0;
+  });
+}
+
+window.addEventListener("scroll", () => {
+  const sectionPos = skillsSection.getBoundingClientRect().top;
+  const screenPos = window.innerHeight / 2;
+
+  if (sectionPos < screenPos) {
+    showProgress();
+  } else {
+    hideProgress();
+  }
+});
